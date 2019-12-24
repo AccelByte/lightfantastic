1ea995997118785d0544cf2c1e98b58f2649dca5
expee105 <experian.elitiawan@gmail.com>
2019-12-24 16:05:19 +0700

    Revamp the netcode
    Now it's not using full server authoritative model,
    instead the client can set it's fields while being
    monitored by the server.
    If there's any suspicious fields value, the server
    will ban the corresponding player.

--- JusticeUnitySampleProject/Assets/Scripts/Gameplay/BaseGameManager.cs
+++ JusticeUnitySampleProject/Assets/Scripts/Gameplay/BaseGameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using BeardedManStudios.Forge.Networking.Generated;
 using BeardedManStudios.Forge.Networking.Unity;
+using BeardedManStudios.Forge.Networking;
 
 namespace Game
 {
@@ -20,8 +21,7 @@ namespace Game
 
         [SerializeField]
         private BasePlayerStart[] playerStarts;
-
-        private readonly Dictionary<uint, MovePlayerPawnBehavior> playerObjects = new Dictionary<uint, MovePlayerPawnBehavior>();        
+        private readonly Dictionary<uint, MovePlayerPawnBehavior> playerObjects = new Dictionary<uint, MovePlayerPawnBehavior>();
         private bool isNetworkReady;
 
         private void Awake()
@@ -34,7 +34,7 @@ namespace Game
             {
                 Destroy(gameObject);
             }
-            
+
             DontDestroyOnLoad(instance);
         }
 
@@ -45,16 +45,20 @@ namespace Game
             if (NetworkManager.Instance.IsServer)
             {
                 NetworkManager.Instance.Networker.playerAccepted += (player, sender) =>
-                  {
-                      MainThreadManager.Run(() =>
-                      {
-                          int randomPlayerStart = Random.Range(0, playerStarts.Length);
-                          MovePlayerPawnBehavior p = NetworkManager.Instance.InstantiateMovePlayerPawn(0,playerStarts[randomPlayerStart].transform.position, playerStarts[randomPlayerStart].transform.rotation);
-                          p.networkObject.OwnerNetId = player.NetworkId;
-                          playerObjects.Add(player.NetworkId, p);
-                          Debug.Log("Player " + player.NetworkId + " connected.");
-                      });
-                  };
+                {
+                    MainThreadManager.Run(() =>
+                    {
+                        int playerIdx = Random.Range(0, playerStarts.Length);
+                        MovePlayerPawnBehavior p = NetworkManager.Instance.InstantiateMovePlayerPawn(0, playerStarts[playerIdx].transform.position, playerStarts[playerIdx].transform.rotation);
+                        p.networkObject.OwnerNetId = player.NetworkId;
+                        p.networkObject.MaxSpeed = ((BasePlayerPawn)p).MaxSpeed;
+                        p.networkObject.SetInitialPos(playerStarts[playerIdx].transform.position);
+                        p.networkObject.AssignOwnership(player);
+                        p.networkObject.SendRpc(player, MovePlayerPawnBehavior.RPC_START_ASSIGN_PLAYER_NUM, (uint)playerIdx + 1);
+                        playerObjects.Add(player.NetworkId, p);
+                        Debug.Log("Player " + player.NetworkId + " connected.");
+                    });
+                };
 
                 NetworkManager.Instance.Networker.playerDisconnected += (player, sender) =>
                 {
@@ -72,14 +76,6 @@ namespace Game
             }
 
             isNetworkReady = true;
-        }
-
-        private void FixedUpdate()
-        {
-            if (!isNetworkReady && networkObject != null)
-            {
-                NetworkStart();
-            }
         }
     }
 }
--- JusticeUnitySampleProject/Assets/Scripts/Gameplay/BasePlayerPawn.cs
+++ JusticeUnitySampleProject/Assets/Scripts/Gameplay/BasePlayerPawn.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using BeardedManStudios.Forge.Networking.Generated;
+using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Unity;
 using Game.Util;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,279 +16,134 @@ namespace Game
     /// </summary>
     public class BasePlayerPawn : MovePlayerPawnBehavior
     {
-        [Tooltip("The movement speed of the character.")]
-        public float movementSpeed = 1.0f;
-        [Tooltip("Representing the model view.")]
-        public GameObject modelView;
-
-        private Rigidbody pawnRigidbody;
-        private CharacterController characterController;
+        private Rigidbody2D rb2d;
         private bool isNetworkReady;
         private bool isInitialized;
-        private bool isLocalOwner;
-
-        private BaseInputFrame currentInput;
-        private BaseInputListener inputListener;
-
-        // last frame was processed locally
-        private uint lastLocalFrame;
-        // last frame that was sent by server / received by  client over the network
-        private uint lastNetworkFrame;
-        // calculates the current error between Simulation and modelView
-        private Vector3 errorVector = Vector3.zero;
-        // The interpolation timer for error interpolation
-        private float errorTimer;
-
+        private bool isPlayerNumAssigned;
         private BaseHoveringText hoveringText;
-
-        private const float MAGTITUDE_THRESHOLD = 0.00001f;
+        [SerializeField]
+        private float speedDecayConst;
+        [SerializeField]
+        private float speedIncreaseConst;
+        [SerializeField]
+        private float maxSpeed_;
+        private float currSpeed;
+        public float MaxSpeed { get { return maxSpeed_; } }
 
         private void Awake()
         {
-            characterController = GetComponent<CharacterController>();
-            pawnRigidbody = GetComponentInChildren<Rigidbody>();
+            rb2d = GetComponent<Rigidbody2D>();
             hoveringText = GetComponent<BaseHoveringText>();
+            currSpeed = 0.0f;
         }
 
         protected override void NetworkStart()
         {
             base.NetworkStart();
             isNetworkReady = true;
+            StartCoroutine("PlayerInitialization");
         }
 
-        /// <summary>
-        ///     Initialization, sets all the initial parameters
-        /// </summary>
-        /// <returns></returns>
         private bool Initialize()
         {
-            if (!networkObject.IsServer)
-            {
-                if (networkObject.OwnerNetId == 0)
-                {
-                    isInitialized = false;
-                    return isInitialized;
-                }
-            }
-
-            isLocalOwner = networkObject.MyPlayerId == networkObject.OwnerNetId;
-
-            if (isLocalOwner || networkObject.IsServer)
-            {
-                networkObject.PositionInterpolation.Enabled = false;
-                networkObject.RotationInterpolation.Enabled = false;
-                if (inputListener == null)
-                {
-                    inputListener = FindObjectsOfType<BaseInputListener>()
-                        .FirstOrDefault(x => x.networkObject.Owner.NetworkId == networkObject.OwnerNetId);
-                    if (inputListener == null)
-                    {
-                        isInitialized = false;
-                        return isInitialized;
-                    }
-                }
-            }
-
-            // TODO: Change this into username / character name
-            hoveringText.ChangeTextLabel("Player " + networkObject.OwnerNetId.ToString());
-
+            Debug.Log("Start Initialization");
+            networkObject.OwnerNetId = networkObject.MyPlayerId;
+            networkObject.MaxSpeed = maxSpeed_;
             isInitialized = true;
             return isInitialized;
         }
 
-
-        /// <summary>
-        ///     Handles network sync and update of entityState
-        /// </summary>
-        // Update is called once per frame
         void Update()
         {
-            if (!isNetworkReady || !isInitialized)
+            if (!isNetworkReady || !isInitialized || networkObject.Banned)
             {
                 return;
             }
-
-            // Set the networked fields in update so the pawn up to date the last physics update
-            if (networkObject.IsServer)
+            if (!networkObject.IsOwner)
             {
-                if (lastNetworkFrame < lastLocalFrame)
-                {
-                    networkObject.Position = pawnRigidbody.position;
-                    networkObject.Rotation = pawnRigidbody.rotation;
-
-                    lastNetworkFrame = lastLocalFrame;
-                    networkObject.Frame = lastLocalFrame;
-                }
-            }
-            // The local player has to smooth away some errors
-            else if (isLocalOwner)
-            {
-                CorrectError();
-            }
-            // Update frame nunbers and authoritatively set position if It's a remote player
-            else
-            {
-                lastLocalFrame = lastNetworkFrame = networkObject.Frame;
-                modelView.transform.position = new Vector3(pawnRigidbody.position.x,pawnRigidbody.position.y,modelView.transform.position.z);
-            }
-        }
-
-        /// <summary>
-        ///     Handles prediction, server processing , reconciliation & fixed udpate of state
-        /// </summary>
-        void FixedUpdate()
-        {
-            if (!isNetworkReady)
-            {
+                transform.position = networkObject.Position;
                 return;
             }
-            if (!isInitialized && !Initialize())
-            {
-                return;
-            }
-            if ((networkObject.IsServer || isLocalOwner) && inputListener == null)
-            {
-                return;
-            }
-
-            if (!networkObject.IsServer)
-            {
-                pawnRigidbody.position = networkObject.Position;
-                pawnRigidbody.rotation = networkObject.Rotation.normalized;
-                if (isLocalOwner && networkObject.Frame != 0 && lastNetworkFrame <= networkObject.Frame)
-                {
-                    lastNetworkFrame = networkObject.Frame;
-                    ValidateFrames();
-                }
-            }
-
-            if (!isLocalOwner && !networkObject.IsServer)
-            {
-                return;
-            }
-
-            // Local client prediction and server auhoritative logic
-            if (inputListener.framesToPlay.Count <= 0)
-            {
-                return;
-            }
-            currentInput = inputListener.framesToPlay.Pop();
-            lastLocalFrame = currentInput.frameNumber;
-
-            // Try to do a player update
-            try
-            {
-                PlayerUpdate(currentInput);                
-            }
-            catch(Exception e)
-            {
-                Debug.LogError("BasePlayerPawn::FixedUpdate() Malformed input frame.");
-                Debug.LogError(e);
-            }
-
-            inputListener.framesToValidate.Add(currentInput);
+            ReceiveInput(Time.deltaTime);
         }
 
-        /// <summary>
-        ///     Move the player's pawn simulation (rigid body)
-        /// </summary>
-        /// <param name="input"></param>
-        private void Move(BaseInputFrame input)
+        public override void StartInitialization(RpcArgs args)
         {
-            // Move the player, clamping the movement so diagonals aren't faster
-            //Vector3 movement = Vector3.ClampMagnitude(new Vector3(input.horizontal, input.vertical, 0) * movementSpeed * Time.fixedDeltaTime, movementSpeed);
-            Vector3 movement = new Vector3(input.horizontal, 0, input.vertical) * movementSpeed * Time.fixedDeltaTime;
-
-            pawnRigidbody.MovePosition(pawnRigidbody.position + movement);
         }
 
-        private void Rotate(BaseInputFrame input)
+        public override void StartAssignPlayerNum(RpcArgs args)
         {
-            // Determine the number of degrees to be turned based on the input, speed and time between frames.
-            float turn = input.horizontal * 40 * Time.fixedDeltaTime;
-
-            // Make this into a rotation in the y axis.
-            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
-
-            // Apply this rotation to the rigidbody's rotation.
-            pawnRigidbody.MoveRotation(pawnRigidbody.rotation * turnRotation);
-        }
-
-        /// <summary>
-        ///     Detect and Resolve collisions with a simple on trigger overlap check
-        /// </summary>
-        private void PhysicsCollisions()
-        {
-            // If not moving then ignore
-            if (pawnRigidbody.velocity == Vector3.zero)
+            MainThreadManager.Run(() =>
             {
-                return;
-            }
+                AssignPlayerNum(args.GetAt<uint>(0));
+            });
         }
 
-        /// <summary>
-        ///     Player update composed of movement and collision processing
-        /// </summary>
-        /// <param name="input"></param>
-        private void PlayerUpdate(BaseInputFrame input)
+        public override void Ban(RpcArgs args)
         {
-            pawnRigidbody.velocity = Vector3.zero;
-            if (input != null)// && input.hasInput) // since input only happens on key down, hasInput is not relevant
-            {
-                Move(input);
-                PhysicsCollisions();
-            }
+            Debug.Log("Player Got BanHammered");
+            MainThreadManager.Run(() => { networkObject.Banned = true; });
         }
 
-        /// <summary>
-        ///     Validate inputs that haven't yet been authoritaatively processed by server
-        /// </summary>
-        private void ValidateFrames()
+        public override void UpdatePosition(RpcArgs args)
         {
-            // Remove any inputs up to and including the last input processed by the server
-            inputListener.framesToValidate.RemoveAll(f => f.frameNumber < networkObject.Frame);
-
-            // Replay them all back to the last input processed by client prediction
-            if(inputListener.framesToValidate.Count > 0)
-            {
-                for (Int32 i = 0; i  < inputListener.framesToValidate.Count; ++i)
-                {
-                    currentInput = inputListener.framesToValidate[i];
-                    PlayerUpdate(currentInput);
-                }
-            }
-
-            // The error vector measures the diff between the predicted and server position
-            // and the view position (renderer view)
-            errorVector = pawnRigidbody.position - (Vector3)modelView.transform.position;
-            errorTimer = 0f;
+            MainThreadManager.Run(() => { transform.position = networkObject.Position; });
         }
 
-        private void CorrectError()
+        public void ReceiveInput(float dt)
         {
-            if (errorVector.magnitude >= MAGTITUDE_THRESHOLD)
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
             {
-                float weight = Math.Max(0.0f, .75f - errorTimer);
-
-                Vector3 newViewPosition = (Vector3)modelView.transform.position * weight + pawnRigidbody.position * (1.0f - weight);
-                modelView.transform.position = new Vector3(newViewPosition.x,newViewPosition.y,modelView.transform.position.z);
-
-                errorTimer += Time.fixedDeltaTime;
-
-                errorVector = pawnRigidbody.position - (Vector3)modelView.transform.position;
-
-                if (!(errorVector.magnitude < MAGTITUDE_THRESHOLD))
-                {
-                    return;
-                }
-
-                errorVector = Vector3.zero;
-                errorTimer = 0.0f;
+                currSpeed += speedIncreaseConst;
             }
             else
             {
-                modelView.transform.position = new Vector3(pawnRigidbody.position.x,pawnRigidbody.position.y,modelView.transform.position.z);
+                currSpeed = LinearDecay(currSpeed, dt);
             }
+            Vector3 newPos = transform.position;
+            newPos.x = transform.position.x + currSpeed;
+            if (networkObject.IsOwner)
+            {
+                networkObject.SendRpc(RPC_UPDATE_POSITION, Receivers.Others, newPos);
+                transform.position = networkObject.Position = newPos;
+            }
+        }
+
+        private void AssignPlayerNum(uint newPlayerNum)
+        {
+            if (networkObject.IsOwner)
+            {
+                networkObject.playerNum = newPlayerNum;
+            }
+            hoveringText.ChangeTextLabel("Player " + newPlayerNum);
+            isPlayerNumAssigned = true;
+        }
+
+        public float LinearDecay(float inVal, float dt)
+        {
+            inVal -= speedDecayConst * dt;
+            if (inVal < 0)
+            {
+                inVal = 0;
+            }
+            return inVal;
+        }
+        IEnumerator PlayerInitialization()
+        {
+            while (!isInitialized || !isPlayerNumAssigned)
+            {
+                if (!isInitialized)
+                {
+                    Initialize();
+                }
+                if (!networkObject.IsOwner && networkObject.playerNum != 0 && !isPlayerNumAssigned)
+                {
+                    AssignPlayerNum(networkObject.playerNum);
+                }
+                yield return null;
+            }
+            networkObject.Banable = true;
+            Debug.Log("Player Initialization Complete");
         }
     }
 }
