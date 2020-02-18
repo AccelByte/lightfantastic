d6dde2db1e8f29fa657fe021fbc500a22751f9d8
JonathanEka <jonathan@accelbyte.net>
2020-02-18 15:26:41 +0700

    * Add forgotten helper

---
+++ JusticeUnitySampleProject/Assets/Scripts/Helper/DeveloperConsoleHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeveloperConsoleHelper : MonoBehaviour
+{
+    
+    private static DeveloperConsoleHelper instance_;
+    public static DeveloperConsoleHelper Instance
+    {
+        get
+        {
+            if (instance_ == null)
+                CreateGameObject();
+            return instance_;
+        }
+    }
+    
+    private void Awake()
+    {
+        if (instance_ != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance_ = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private static void CreateGameObject()
+    {
+        if (instance_ == null)
+        {
+            new GameObject("Developer Console Helper").AddComponent<DeveloperConsoleHelper>();
+        }
+    }
+    
+    public void Refresh()
+    {
+        if (!LightFantasticConfig.DEVELOPER_CONSOLE_VISIBLE)
+        {
+            MainThreadTaskRunner.Instance.Run(()=>{StartCoroutine(ClearConsoleAsync());});
+        }
+    }
+
+    IEnumerator ClearConsoleAsync()
+    {
+        while(!Debug.developerConsoleVisible)
+        {
+            yield return null;
+        }
+        yield return null;
+        Debug.ClearDeveloperConsole();
+    }
+}
