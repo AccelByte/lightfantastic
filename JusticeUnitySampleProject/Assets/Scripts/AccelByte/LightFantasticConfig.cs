9c40138d782667d155bf9d284849027c665467b4
richard <richard@accelbyte.net>
2020-12-21 13:57:27 +0700

    feat(version): update SDK config version

--- JusticeUnitySampleProject/Assets/Scripts/AccelByte/LightFantasticConfig.cs
+++ JusticeUnitySampleProject/Assets/Scripts/AccelByte/LightFantasticConfig.cs
@@ -5,7 +5,7 @@ public static class LightFantasticConfig
 {
     public const string GAME_VERSION = "0.0.10";
     public const string DS_TARGET_VERSION = "v0.0.6";
-    public const string SDK_VERSION = "2.24.0";
+    public const string SDK_VERSION = "2.25.0";
     
     public const string DEFAULT_LANGUAGE = "en";
     public const string IMAGE_AS = "product-cover";
