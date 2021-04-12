d32c865c52ea3627dfef426fc776bb607272bbab
dhani <dhani@accelbyte.net>
2021-04-12 13:17:29 +0700

    Update SDK 2.29.0
    Changes :
    - Change how to receive service type(from enum to string) and delete logic checking with enum.

--- JusticeUnitySampleProject/Assets/Scripts/AccelByte/LightFantasticConfig.cs
+++ JusticeUnitySampleProject/Assets/Scripts/AccelByte/LightFantasticConfig.cs
@@ -4,8 +4,8 @@ using AccelByte.Api;
 public static class LightFantasticConfig
 {
     public const string GAME_VERSION = "0.0.13";
-    public const string DS_TARGET_VERSION = "v0.0.13c";
-    public const string SDK_VERSION = "2.28.1";
+    public const string DS_TARGET_VERSION = "v0.0.13d";
+    public const string SDK_VERSION = "2.29.0";
     
     public const string DEFAULT_LANGUAGE = "en";
     public const string IMAGE_AS = "product-cover";
